9c00fd92ee43eb0cc3cd87bf5b39952f021b90d0
denis-spe <denisbrian07@gmail.com>
2023-04-19 00:04:16 +0200

    Added a before logged in ud=ser fuction

--- auth.py
+++ auth.py
@@ -75,8 +75,20 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('idex'))
+            return redirect(url_for('index'))
 
         flash(error)
 
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?',
+            (user_id)
+        ).fetchone()
